afe10065617c4cad61dbaea9b2b9753dd16903f0
Matthew <Mattpet26@gmail.com>
2020-07-14 13:39:12 -0700

    Changed object literal to constructor

--- app.js
+++ app.js
@@ -8,6 +8,80 @@ function getRandomArbitrary(min, max) {
   return Math.floor(Math.random() * (max - min)) + min;
 }
 
+function cookieFunction(minNum, maxNum, avgCookie, totalCookies, city, time){
+  this.min = minNum;
+  this.max = maxNum;
+  this.avgCookie = avgCookie;
+  this.totalCookies = totalCookies;
+  this.city = city;
+  this.time = time;
+  this.renderCity = function(){
+    var cityList = document.getElementById('city');
+    var location = document.createElement('li');
+    location.textContent = this.city;
+    cityList.appendChild(location);
+  },
+  this.renderTime = function(){
+    for(var i = 0; i < this.time.length; i++){
+      var cookieSale = getRandomArbitrary(23,65);
+      this.totalCookies = this.totalCookies + cookieSale;
+      var timeList = document.getElementById('seattleCookies');
+      var timeLine = document.createElement('li');
+      timeLine.textContent = this.time[i] + cookieSale + ' cookies';
+      timeList.appendChild(timeLine)
+    }
+    var cookieTotalEl = document.getElementById('SeaTotal');
+      var LiEl = document.createElement('li');
+      LiEl.textContent = this.totalCookies + ' Total!';
+      cookieTotalEl.appendChild(LiEl)
+      console.log(this.max)
+  }
+}
+
+var seattleName = new cookieFunction(23, 65, 6.3, 0, 'Seattle', ['6am: ', '7am: ', '8am: ', '9am: ', '10am: ', '11am: ', '12am: ', '1pm: ', '2pm: ', '3pm: ', '4pm: ', '5pm: ', '6pm: ', '7pm: ']);
+seattleName.renderCity();
+seattleName.renderTime();
+
+var tokyoName = new cookieFunction(23, 65, 6.3, 0, 'Tokyo', ['6am: ', '7am: ', '8am: ', '9am: ', '10am: ', '11am: ', '12am: ', '1pm: ', '2pm: ', '3pm: ', '4pm: ', '5pm: ', '6pm: ', '7pm: ']);
+tokyoName.renderCity();
+tokyoName.renderTime();
+
+var dubaiName = new cookieFunction(23, 65, 6.3, 0, 'Dubai', ['6am: ', '7am: ', '8am: ', '9am: ', '10am: ', '11am: ', '12am: ', '1pm: ', '2pm: ', '3pm: ', '4pm: ', '5pm: ', '6pm: ', '7pm: ']);
+dubaiName.renderCity();
+dubaiName.renderTime();
+
+var parisName = new cookieFunction(23, 65, 6.3, 0, 'Paris', ['6am: ', '7am: ', '8am: ', '9am: ', '10am: ', '11am: ', '12am: ', '1pm: ', '2pm: ', '3pm: ', '4pm: ', '5pm: ', '6pm: ', '7pm: ']);
+parisName.renderCity();
+parisName.renderTime();
+
+var limaName = new cookieFunction(23, 65, 6.3, 0, 'Lima', ['6am: ', '7am: ', '8am: ', '9am: ', '10am: ', '11am: ', '12am: ', '1pm: ', '2pm: ', '3pm: ', '4pm: ', '5pm: ', '6pm: ', '7pm: ']);
+limaName.renderCity();
+limaName.renderTime();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*
+function getRandomArbitrary(min, max) {
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  return Math.floor(Math.random() * (max - min)) + min;
+}
+
 var SeattleCookies = {
   min: 23, 
   max: 65,
@@ -171,3 +245,5 @@ var LimaCookies = {
 }
 LimaCookies.renderTime();
 LimaCookies.renderCity();
+
+*/
