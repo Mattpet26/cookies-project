88999df08d3ef37e270d35486e824771fc5a4ec1
Matthew <Mattpet26@gmail.com>
2020-07-14 17:45:09 -0700

    fixxed all javascript problems

--- app.js
+++ app.js
@@ -8,28 +8,41 @@ function getRandomArbitrary(min, max) {
   return Math.floor(Math.random() * (max - min)) + min;
 }
 
-function cookieFunction(minNum, maxNum, avgCookie, totalCookies, city, time){
-  this.min = minNum;
-  this.max = maxNum;
+function getRandomArbitrary(min, max) {
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  return Math.floor(Math.random() * (max - min)) + min;
+}
+
+function cookieFunction(min, max, avgCookie, totalCookies, city){
+  this.min = min;
+  this.max = max;
   this.avgCookie = avgCookie;
   this.totalCookies = totalCookies;
   this.city = city;
-  this.time = time;
+  this.cookieArray = [],
+  this.time = ['6am: ', '7am: ', '8am: ', '9am: ', '10am: ', '11am: ', '12am: ', '1pm: ', '2pm: ', '3pm: ', '4pm: ', '5pm: ', '6pm: ', '7pm: '];
   this.renderCity = function(){
     var cityList = document.getElementById('city');
     var location = document.createElement('li');
     location.textContent = this.city;
     cityList.appendChild(location);
-  },
+  }
+  this.renderMath = function(){
+    for(var i = 0; i < this.time.length; i++){
+      var custPerHr = getRandomArbitrary(this.min, this.max);
+      var cookiesThisHr = Math.floor(custPerHr * this.avgCookie);
+      this.cookieArray.push(cookiesThisHr);
+      this.totalCookies = this.totalCookies + cookiesThisHr;
+    }
   this.renderTime = function(){
     for(var i = 0; i < this.time.length; i++){
-      var cookieSale = getRandomArbitrary(23,65);
-      this.totalCookies = this.totalCookies + cookieSale;
-      var timeList = document.getElementById('seattleCookies');
+      var timeList = document.getElementById('city');
       var timeLine = document.createElement('li');
-      timeLine.textContent = this.time[i] + cookieSale + ' cookies';
+      timeLine.textContent = this.time[i] + this.cookieArray[i] + ' cookies';
       timeList.appendChild(timeLine)
     }
+  }
     var cookieTotalEl = document.getElementById('SeaTotal');
       var LiEl = document.createElement('li');
       LiEl.textContent = this.totalCookies + ' Total!';
@@ -37,11 +50,13 @@ function cookieFunction(minNum, maxNum, avgCookie, totalCookies, city, time){
       console.log(this.max)
   }
 }
-
-var seattleName = new cookieFunction(23, 65, 6.3, 0, 'Seattle', ['6am: ', '7am: ', '8am: ', '9am: ', '10am: ', '11am: ', '12am: ', '1pm: ', '2pm: ', '3pm: ', '4pm: ', '5pm: ', '6pm: ', '7pm: ']);
+var seattleName = new cookieFunction(23, 65, 6.3, 0, 'Seattle');
 seattleName.renderCity();
+seattleName.renderMath();
 seattleName.renderTime();
 
+
+/*
 var tokyoName = new cookieFunction(23, 65, 6.3, 0, 'Tokyo', ['6am: ', '7am: ', '8am: ', '9am: ', '10am: ', '11am: ', '12am: ', '1pm: ', '2pm: ', '3pm: ', '4pm: ', '5pm: ', '6pm: ', '7pm: ']);
 tokyoName.renderCity();
 tokyoName.renderTime();
@@ -57,7 +72,7 @@ parisName.renderTime();
 var limaName = new cookieFunction(23, 65, 6.3, 0, 'Lima', ['6am: ', '7am: ', '8am: ', '9am: ', '10am: ', '11am: ', '12am: ', '1pm: ', '2pm: ', '3pm: ', '4pm: ', '5pm: ', '6pm: ', '7pm: ']);
 limaName.renderCity();
 limaName.renderTime();
-
+*/
 
 
 
