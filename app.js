c167195c24e6fd6f01a93a04b64cb11d111b5f84
Matthew <Mattpet26@gmail.com>
2020-07-15 09:28:15 -0700

    added picture and notes to js

--- app.js
+++ app.js
@@ -1,14 +1,8 @@
 'use strict';
-// https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Math/random
 //Pair programmed with Mike and Riva/collaborated with.
 
-function getRandomArbitrary(min, max) {
-  min = Math.ceil(min);
-  max = Math.floor(max);
-  return Math.floor(Math.random() * (max - min)) + min;
-}
-
 function cookieFunction(min, max, avgCookie, totalCookies, city){
+  // this refers to the new object made by the constructor
   this.min = min;
   this.max = max;
   this.avgCookie = avgCookie;
@@ -17,21 +11,24 @@ function cookieFunction(min, max, avgCookie, totalCookies, city){
   this.cookieArray = [],
   this.time = ['6am: ', '7am: ', '8am: ', '9am: ', '10am: ', '11am: ', '12am: ', '1pm: ', '2pm: ', '3pm: ', '4pm: ', '5pm: ', '6pm: ', '7pm: '];
   this.renderCity = function(){
+  //assigning a method to rendercity and it gets a function.
     var table = document.getElementById('tableStuff');
     var row = document.createElement('tr');
     var tableData = document.createElement('td')
     tableData.textContent = this.city;
     row.appendChild(tableData)
-
-    //this is where i start my loop
     for(var i = 0; i < this.time.length; i++){
       var tableData = document.createElement('td')
       tableData.textContent = this.cookieArray[i];
       row.appendChild(tableData)
     }
-
     table.appendChild(row);
-
+  }
+  function getRandomArbitrary(min, max) {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min)) + min;
+    // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Math/random
   }
   this.renderMath = function(){
     for(var i = 0; i < this.time.length; i++){
@@ -58,14 +55,11 @@ function cookieFunction(min, max, avgCookie, totalCookies, city){
     var tableData = document.createElement('td')
     tableData.textContent = 'Totals';
     row.appendChild(tableData)
-
-    //this is where i start my loop
     for(var i = 0; i < this.time.length; i++){
       var tableData = document.createElement('td')
       tableData.textContent = seattleName.cookieArray[i] + tokyoName.cookieArray[i] + dubaiName.cookieArray[i] + parisName.cookieArray[i] + limaName.cookieArray[i];
       row.appendChild(tableData)
     }
-
     table.appendChild(row);
   }
     var cookieTotalEl = document.getElementById('tableStuff');
