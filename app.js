d7d223da179f2063eb89f05f248aef722416896a
Matthew <Mattpet26@gmail.com>
2020-07-15 17:38:50 -0700

    finalized table, started css, added sales

--- app.js
+++ app.js
@@ -10,31 +10,39 @@ function cookieFunction(min, max, avgCookie, totalCookies, city){
   this.city = city;
   this.cookieArray = [],
   this.time = ['6am: ', '7am: ', '8am: ', '9am: ', '10am: ', '11am: ', '12am: ', '1pm: ', '2pm: ', '3pm: ', '4pm: ', '5pm: ', '6pm: ', '7pm: '];
+
+
     //assigning a method to rendercity and it gets a function.
   this.renderCity = function(){
-    //finding the target / parent
     var table = document.getElementById('tableStuff');
-    //creating an element - tr
     var row = document.createElement('tr');
-    //creating an element - td, and giving content to row
     var tableData = document.createElement('td')
-    // giving the tableData content
     tableData.textContent = this.city;
-    // row object representing a tr, and call appendchild method passing in the tableData object representing the td
     row.appendChild(tableData)
+
+
     for(var i = 0; i < this.time.length; i++){
       var tableData = document.createElement('td')
       tableData.textContent = this.cookieArray[i];
       row.appendChild(tableData)
     }
+
+
+    var tableCityTotals = document.createElement('td')
+    tableCityTotals.textContent = this.totalCookies;
+    row.appendChild(tableCityTotals);
     table.appendChild(row);
   }
+
+
   function getRandomArbitrary(min, max) {
     min = Math.ceil(min);
     max = Math.floor(max);
     return Math.floor(Math.random() * (max - min)) + min;
     //source https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Math/random
   }
+
+
   this.renderMath = function(){
     for(var i = 0; i < this.time.length; i++){
       var custPerHr = getRandomArbitrary(this.min, this.max);
@@ -42,35 +50,51 @@ function cookieFunction(min, max, avgCookie, totalCookies, city){
       this.cookieArray.push(cookiesThisHr);
       this.totalCookies = this.totalCookies + cookiesThisHr;
     }
+  }
+
+
   this.renderTime = function(){
+    // this does the hour header
     var table = document.getElementById('tableStuff');
     var row = document.createElement('tr');
     var tableData = document.createElement('td')
     row.appendChild(tableData)
+
+
     for(var i = 0; i < this.time.length; i++){
-      var tableData = document.createElement('td')
+      var tableData = document.createElement('td');
       tableData.textContent = this.time[i];
       row.appendChild(tableData);
     }
-    table.appendChild(row);
+    var tableData = document.createElement('td');
+      tableData.textContent = 'City totals';
+      row.appendChild(tableData);
+     table.appendChild(row);
   }
+
+
   this.renderBottomRow = function(){
     var table = document.getElementById('tableStuff');
     var row = document.createElement('tr');
     var tableData = document.createElement('td')
     tableData.textContent = 'Totals';
     row.appendChild(tableData)
+    var counter = 0;
+
+
     for(var i = 0; i < this.time.length; i++){
       var tableData = document.createElement('td')
-      tableData.textContent = seattleName.cookieArray[i] + tokyoName.cookieArray[i] + dubaiName.cookieArray[i] + parisName.cookieArray[i] + limaName.cookieArray[i];
+      var saveTheCookieTotals = seattleName.cookieArray[i] + tokyoName.cookieArray[i] + dubaiName.cookieArray[i] + parisName.cookieArray[i] + limaName.cookieArray[i];
+      tableData.textContent = saveTheCookieTotals;
+      counter = saveTheCookieTotals + counter;
       row.appendChild(tableData)
     }
+
+
+    var tableData = document.createElement('td')
+    tableData.textContent = counter;
+    row.appendChild(tableData)
     table.appendChild(row);
-  }
-    var cookieTotalEl = document.getElementById('tableStuff');
-      var LiEl = document.createElement('tr');
-      LiEl.textContent = this.totalCookies;
-      cookieTotalEl.appendChild(LiEl)
   }
 }
 var seattleName = new cookieFunction(23, 65, 6.3, 0, 'Seattle');
